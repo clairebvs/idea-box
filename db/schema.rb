ae9aca99a309003a5cd4e79f0df100e0d30721ce
ClaireBvs <clairembeauvais@gmail.com>
2018-07-14 23:41:06 -0600

    Add Ideas migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715053328) do
+ActiveRecord::Schema.define(version: 20180715053935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20180715053328) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "user_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_ideas_on_category_id"
+    t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
   create_table "images", force: :cascade do |t|
@@ -38,4 +49,6 @@ ActiveRecord::Schema.define(version: 20180715053328) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ideas", "categories"
+  add_foreign_key "ideas", "users"
 end
