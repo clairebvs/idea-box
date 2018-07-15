ee573584d63cb1b78bb5835b5170aefee771b59a
ClaireBvs <clairembeauvais@gmail.com>
2018-07-14 23:46:17 -0600

    Add IdeaImage migration

---
+++ app/models/idea_image.rb
@@ -0,0 +1,3 @@
+class IdeaImage < ApplicationRecord
+
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715053935) do
+ActiveRecord::Schema.define(version: 20180715054411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20180715053935) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "idea_images", force: :cascade do |t|
+    t.bigint "idea_id"
+    t.bigint "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_idea_images_on_idea_id"
+    t.index ["image_id"], name: "index_idea_images_on_image_id"
   end
 
   create_table "ideas", force: :cascade do |t|
@@ -49,6 +58,8 @@ ActiveRecord::Schema.define(version: 20180715053935) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "idea_images", "ideas"
+  add_foreign_key "idea_images", "images"
   add_foreign_key "ideas", "categories"
   add_foreign_key "ideas", "users"
 end
