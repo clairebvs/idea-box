f063c5b75bd5b7dfe5874585a35385a0ffa38584
ClaireBvs <clairembeauvais@gmail.com>
2018-07-17 13:49:38 -0600

    Add test registered user wants to log out

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -17,4 +17,19 @@ describe 'login workflow' do
     expect(page).to_not have_content("I already have an account")
   end
 
+  it 'allows registered user to log out' do
+    user = User.create(username: 'Ben09', password: '123', email: 'ben@yahoo.com', country: 'USA')
+
+    visit root_path
+    click_on 'I already have an account'
+    expect(current_path).to eq(login_path)
+    fill_in :username, with: user.username
+    fill_in :password, with: user.password
+
+    click_on 'Log In'
+    click_on 'Log Out'
+    expect(current_path).to eq(root_path)
+    expect(page).to_not have_content('I already have an account')
+  end
+
 end
