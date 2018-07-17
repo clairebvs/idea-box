7fd0b0c871139ef8dc35ffcda488c4b18b269ec6
ClaireBvs <clairembeauvais@gmail.com>
2018-07-16 22:33:37 -0600

    add link to user_ideas index to click to create new idea

--- spec/features/user_can_create_new_idea_spec.rb
+++ spec/features/user_can_create_new_idea_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 describe 'user can create a new idea' do
+  it 'can click on create a new idea on their index user idea page' do
+
+
+
+  end
+
+
   it 'by filling out a form' do
     user = User.create(username: "Elena", password: "12343", email: 'John@yahoo.com', country: 'USA', role: 1)
     category = Category.create(name: "World", created_at: 2017-02-10, updated_at: 2017-10-12)
