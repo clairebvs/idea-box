e8ee8642eebf91c5b1a9187ac53c578da2261bf9
ClaireBvs <clairembeauvais@gmail.com>
2018-07-17 16:32:03 -0600

    Add new test default user cannot see categories index and refactor test

--- spec/features/admin_creates_new_category_spec.rb
+++ spec/features/admin_creates_new_category_spec.rb
@@ -2,15 +2,6 @@ require 'rails_helper'
 
 describe 'User visits categories index page' do
   context 'as admin' do
-    it 'can see all categories index' do
-      admin = User.create(username: 'Eli', password: 'password', email: 'eli@yahoo.com', country: 'Australia', role: 1)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
-      visit admin_categories_path
-      expect(page).to have_content('Admin Categories')
-    end
-
     it 'allows admin to create new category' do
       admin = User.create(username: 'Yael', password: '1234', role: 1)
       category = Category.create(name: 'Finance', created_at: 2018-02-03, updated_at: 2018-03-04)
