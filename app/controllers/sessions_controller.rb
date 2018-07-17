fba6fe1ef30f7ffd7326a7d8507dc3798f9a4da9
ClaireBvs <clairembeauvais@gmail.com>
2018-07-17 12:57:30 -0600

    add create action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
 
   def new
-  end 
+  end
+
+  def create
+    @user = User.find_by_username(params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Username and/or password are not valid"
+      render :new
+    end
+  end
 end
