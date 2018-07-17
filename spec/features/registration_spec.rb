b9f5c2232de8fe9774fa72359e1efea22cece7fa
ClaireBvs <clairembeauvais@gmail.com>
2018-07-17 11:23:52 -0600

    Add 'sad' path test for user registration

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -20,4 +20,25 @@ describe 'registration workflow' do
     expect(current_path).to eq(user_path(User.last))
     expect(page).to have_content("Welcome, #{username}!")
   end
+
+  it 'submits registration form with duplicate username' do
+    username = "ben04"
+    User.create(username: username, password: '123098', email: 'ben@yahoo.com', country: 'USA')
+
+    visit root_path
+
+    click_on 'Sign Up to be a New User'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: '12345'
+    fill_in :user_email, with: 'ben@yahoo.com'
+    fill_in :user_country, with: 'USA'
+
+    click_on 'Create User'
+
+    expect(current_path).to eq(users_path)
+  end
+
 end
