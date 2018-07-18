3269f58450b01697e6fc4300dcb11884c0899bf5
ClaireBvs <clairembeauvais@gmail.com>
2018-07-17 20:08:16 -0600

    refactor folder spec feature structure

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,6 +2,7 @@ class Admin::CategoriesController < Admin::BaseController
   before_action :require_admin
 
   def index
+    @categories = Category.all
   end
 
   def new
@@ -9,7 +10,7 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def create
-    @category = Category.new(category_params)
+    @category = @admin.category.new(category_params)
     if @category.save
       redirect_to admin_categories_path
     else
