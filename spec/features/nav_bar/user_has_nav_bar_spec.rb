c658f3d519a913d741f952b1aec189df8a32f0b2
ClaireBvs <clairembeauvais@gmail.com>
2018-07-18 19:48:12 -0600

    Add link nav bar to images for admin

--- spec/features/nav_bar/user_has_nav_bar_spec.rb
+++ spec/features/nav_bar/user_has_nav_bar_spec.rb
@@ -29,4 +29,21 @@ describe 'user visits app' do
 
     expect(page).to_not have_content('Categories')
   end
+
+describe 'admin visits app' do
+  it 'sees a nav bar on top with images link' do
+    admin = User.create(username: "Admin", password: "12343", email: 'admin@yahoo.com', country: 'USA', role: 1)
+    category = Category.create(name: "World", created_at: 2017-02-10, updated_at: 2017-10-12)
+    idea = admin.ideas.create(title: "Travel around the world", description: "Backpack", category_id: category.id, created_at: 2018-02-03, updated_at: 2018-03-04)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit user_path(admin)
+
+    expect(page).to have_content('Images')
+    click_link 'Images'
+
+    expect(current_path).to eq(admin_images_path)
+  end
+end
 end
