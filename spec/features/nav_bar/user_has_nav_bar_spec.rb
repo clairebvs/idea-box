2d5ef29e6aec2e566c992aded8acac52389797e0
ClaireBvs <clairembeauvais@gmail.com>
2018-07-18 19:52:29 -0600

    Add test user cannot see link Images on NavBar

--- spec/features/nav_bar/user_has_nav_bar_spec.rb
+++ spec/features/nav_bar/user_has_nav_bar_spec.rb
@@ -46,4 +46,18 @@ describe 'admin visits app' do
     expect(current_path).to eq(admin_images_path)
   end
 end
+
+describe 'user visits app' do
+  it 'does not see a nav bar on top with images link' do
+    user = User.create(username: "Admin", password: "12343", email: 'admin@yahoo.com', country: 'USA', role: 0)
+    category = Category.create(name: "World", created_at: 2017-02-10, updated_at: 2017-10-12)
+    idea = user.ideas.create(title: "Travel around the world", description: "Backpack", category_id: category.id, created_at: 2018-02-03, updated_at: 2018-03-04)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    expect(page).to_not have_content('Images')
+  end
+end
 end
